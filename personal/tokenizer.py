fc86428cf1f6982d3fb32f770427e217bf61665b
Michael Overman <60492680+TheYoungProfessional@users.noreply.github.com>
2026-01-21 22:02:48 +0000

    added tokenizer patterns

--- personal/tokenizer.py
+++ personal/tokenizer.py
@@ -4,5 +4,32 @@ p = re.compile("ab*")
 
 if p.match("a") :
     print("match")
-else
+else :
     print("not match")
+
+patterns = [
+    #parentheses signify an immutable list
+    (r"\s+", "whitespace"),
+    (r"\d+", "number"),
+    (r"\+", "+"),
+    (r"\-", "-"),
+    (r"\/", "/"),
+    (r"\*", "*"),
+    (r".", "error")
+]
+
+patterns = [(re.compile(p), tag) for p,tag in patterns]
+
+def tokenize(characters):
+    "Tokenize a string using the patterns above"
+    tokens = []
+    position = 0
+    line = 1
+    column = 1
+
+    while position < len(characters):
+        for pattern, tag in patterns:
+            match = pattern.match(characters, position)
+            if match:
+                break
+        assert match is not None
